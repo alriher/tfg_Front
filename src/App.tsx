eada8a5863904c7a2c3835771ecf06714baadbff
alriher <alriher@alumni.uv.es>
2024-06-25 19:41:24 +0200

    chore: Remove unused imports and update styling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react'
 
